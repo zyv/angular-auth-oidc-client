36ac728d910d0eef573775dbe01f6dd86c9ea934
Yury V. Zaytsev <yury@shurup.com>
2024-11-19 10:08:29 +0100

    Adjust configuration for YouTrack

--- projects/sample-implicit-flow-silent-renew/src/app/auth-config.module.ts
+++ projects/sample-implicit-flow-silent-renew/src/app/auth-config.module.ts
@@ -5,15 +5,23 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
   imports: [
     AuthModule.forRoot({
       config: {
-        authority: 'https://offeringsolutions-sts.azurewebsites.net',
+        authority: 'https://xyzzy-gmbh.youtrack.cloud/hub',
         redirectUrl: window.location.origin,
         postLogoutRedirectUri: window.location.origin,
-        clientId: 'angularImplicitClient',
-        scope: 'openid profile email',
+        clientId: '8ef93e4a-bcc7-4c9f-9ac5-3a5124868e10',
+        scope: 'YouTrack 0-0-0-0-0',
         responseType: 'id_token token',
         silentRenewUrl: `${window.location.origin}/silent-renew.html`,
         startCheckSession: true,
         silentRenew: true,
+
+        customParamsAuthRequest: {
+          request_credentials: 'silent',
+          rnd: Math.random().toString(),
+        },
+
+        disableIdTokenValidation: true, // HUB-11902
+
         logLevel: LogLevel.Debug,
       },
     }),
